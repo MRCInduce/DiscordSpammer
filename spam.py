61a63c8e34cc98fafcb6e072ae011b936018fbd6
MRCInduce <38447768+MRCInduce@users.noreply.github.com>
2018-04-16 22:59:37 -0400

    Update spam.py
    Please Dont edit if i dont say so

--- spam.py
+++ spam.py
@@ -1,18 +1,28 @@
 import requests
-i = int(input('Enter A Number:'))
-webhook = "Your WebHook" 
-amount_of_messages = 0 #dont change
-message = " " #dont change
-while amount_of_messages < i:
-    amount_of_messages += 1
-    message += "Your message" + "\n"
-discordInter = {
-    "username": "WhatEverName You want",
-    "content": message #dont change
-}
 
-myHeaders = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0"
-}
 
-Respone = requests.post(webhook, headers=myHeaders, json=discordInter)
+def Spam():
+    i = int(input('Enter A Number:'))
+    webhook = "YOUR WEBHOOK HERE"
+    amount_of_messages = 0
+    message = " "
+    while amount_of_messages < i:
+        amount_of_messages += 1
+        message += "PLEASE INPUT UR MESSAGE HERE" + "\n"
+    discordData = {
+        "username": "",
+        "content": message
+    }
+
+    myHeaders = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0"
+    }
+
+    discordResponse = requests.post(webhook, headers=myHeaders, json=discordData)
+
+
+print("Input How Many Times You Wanna Loop")
+loop = int(input('Enter A Number Smaller Then 5'))
+destroy = 0
+while destroy < loop:
+    Spam()
